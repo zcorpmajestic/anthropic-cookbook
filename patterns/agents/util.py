6b325c0736c409c62a61e76948917862e8cdad7d
alfa <alfazcorp@outlook.com>
2025-05-30 16:08:29 +0100

    converted to use azure opeani, created pure py file

--- patterns/agents/util.py
+++ patterns/agents/util.py
@@ -1,9 +1,11 @@
-from anthropic import Anthropic
-import os
 import re
+import os
+from openai import AzureOpenAI
+from azure.core.credentials import AzureKeyCredential
+from dotenv import load_dotenv
+import os
 
-client = Anthropic(api_key=os.environ["ANTHROPIC_API_KEY"])
-
+load_dotenv()
 def llm_call(prompt: str, system_prompt: str = "", model="claude-3-5-sonnet-20241022") -> str:
     """
     Calls the model with the given prompt and returns the response.
@@ -16,16 +18,19 @@ def llm_call(prompt: str, system_prompt: str = "", model="claude-3-5-sonnet-2024
     Returns:
         str: The response from the language model.
     """
-    client = Anthropic(api_key=os.environ["ANTHROPIC_API_KEY"])
-    messages = [{"role": "user", "content": prompt}]
-    response = client.messages.create(
-        model=model,
-        max_tokens=4096,
-        system=system_prompt,
-        messages=messages,
-        temperature=0.1,
+
+    client = AzureOpenAI(
+    api_version="2024-12-01-preview",
+    azure_endpoint="https://glasgow-innovation-poc-llm.openai.azure.com/",
+    api_key=os.environ["API_KEY"]
     )
-    return response.content[0].text
+
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        messages=messages,
+        model="gpt-4o"
+    )
+    return response.choices[0].message.content
 
 def extract_xml(text: str, tag: str) -> str:
     """
